41d1187049e2fc2ce931f1c6c6812f4f7de20f44
VortexHD <vortexhdwhite@gmail.com>
2021-05-03 12:57:31 +0100

    h830: Dot-ify

--- dot_h830.mk
+++ dot_h830.mk
@@ -18,15 +18,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from h830 device
 $(call inherit-product, device/lge/h830/device.mk)
 
+# DotOS variables
+#WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h830
-PRODUCT_NAME := lineage_h830
+PRODUCT_NAME := dot_h830
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H830
 PRODUCT_MANUFACTURER := LGE
